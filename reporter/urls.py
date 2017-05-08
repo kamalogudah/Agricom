c4ad3b52d90ef8b0df30e1dfdea5e50fd89be927
kamalogudah <mcpaul2058@gmail.com>
2017-05-08 17:26:00 +0300

    Popups in Geodjango

--- reporter/urls.py
+++ reporter/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include,url
-from views import HomePageView, sublocation_datasets
+from views import HomePageView, sublocation_datasets, incidence_datasets
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name = 'home'),
      url(r'^sublocations_data/$', sublocation_datasets, name = 'subloc'),
-      url(r'^incidence_data/$', HomePageView.as_view(), name = 'home'),
+      url(r'^incidence_data/$', incidence_datasets, name = 'name'),
    
 ]
--- reporter/views.py
+++ reporter/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.core.serializers import serialize
 from django.http import HttpResponse
-from models import Sublocations
+from models import Sublocations, Incidences 
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -14,3 +14,7 @@ class HomePageView(TemplateView):
 def sublocation_datasets(request):
     sublocations = serialize('geojson', Sublocations.objects.all())
     return HttpResponse(sublocations, content_type ='json')
+
+def incidence_datasets(request):
+    incidences = serialize('geojson', Incidences.objects.all())
+    return HttpResponse(incidences, content_type= 'json')
